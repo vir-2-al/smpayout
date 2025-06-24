7062fcd064e6ae359ee80df9c31428b9e7c54610
vir-2-al <sa2rn@icloud.com>
2025-06-24 22:34:41 +0300

    + thread

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import logging
-import time
-import keyboard
 import re
+import time
+import logging
 from platform import system
+from threading import Thread
 from typing import Union, Optional
 
 from smdevice.cfg import (SSP_DEFAULT_CURRENCY,
@@ -10,9 +10,11 @@ from smdevice.cfg import (SSP_DEFAULT_CURRENCY,
 from smdevice.device import Device
 from smdevice.device_def import SSPResponse, DeviceCallbackEvents
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger()
 
+is_app_exit = False
 itl_dev_port = None
 current_platform = system()
 if current_platform == 'Windows':
@@ -39,79 +41,94 @@ def get_number(string_number: str) -> Optional[int]:
     m = re.search(r'\d+$', string_number)
     return int(m.group(0).strip()) if m else None
 
-def sample():
-    global device
 
-    # Создание объекта устройства
-    device = Device(event_callback=device_events_callback,  port=itl_dev_port)
-    print(f'Используемый порт: {itl_dev_port}')
-    print(f'Нажмите "E" для включение приема банкнот')
-    print(f'Нажмите "D" для выключения приема банкнот')
-    print(f'Нажмите "P" для выдачи суммы')
-    print(f'Нажмите "Z" для сброса банкнот свыше установленного количества')
-    print(f'Нажмите "R" для отчета о состоянии устройства')
-    print(f'Нажмите "X" для сброса всех банкнот в кассету')
-    print()
-    print(f'Нажмите "Esc" для выхода из программы')
-
-    while not keyboard.is_pressed('esc'):
+def worker(dev: Device):
+    while not is_app_exit:
         try:
-            if not device.connected():
+            if not dev.connected():
                 # Подключение к устройству, установление шифрованного канала связи и инициализация
-                device.connect()
+                dev.connect()
+
             else:
-                # Включение приема банкнот
-                if keyboard.is_pressed('E'):
-                    print('Включение приема банкнот')
-                    device.sspEnableValidator()
-
-                # Выключение приема банкнот
-                if keyboard.is_pressed('D'):
-                    print('Выключение приема банкнот')
-                    device.sspDisableValidator()
-
-                # Выдача суммы
-                if keyboard.is_pressed('P'):
-                    amount = get_number(input('Введите сумму для выдачи: '))
-                    print(f'Выдача суммы: {amount}')
-                    device.sspPayout(amount, SSP_DEFAULT_CURRENCY)
-
-                # Сброс банкноты
-                if keyboard.is_pressed('Z'):
-                    keyboard.clear_all_hotkeys()
-                    amount = get_number(input('Введите номинал банкноты: '))
-                    count = get_number(input('Введите количество банкнот для сдачи: '))
-                    device.sspFloatByDenomination({amount: count})
-
-                # Отчет о состоянии устройства
-                if keyboard.is_pressed('R'):
-                    for banknote_nominal in SSP_DEFAULT_ACCEPT_LIMIT:
-                        # Запрос количества банкнот для сдачи
-                        res, counter = device.sspGetNoteAmount(banknote_nominal, SSP_DEFAULT_CURRENCY)
-                        if res == SSPResponse.ok:
-                           print(f'Количество банкнот {banknote_nominal} {SSP_DEFAULT_CURRENCY}: {counter}')
-                    # Запрос информации о последней операции перемещения банкнот в кассету
-                    res, note_info = device.sspGetCashboxPayoutOpData()
-                    if res == SSPResponse.ok:
-                        for banknote_nominal, banknote_count in note_info.items():
-                            print(f'Количество банкнот {banknote_nominal} {SSP_DEFAULT_CURRENCY}: {banknote_count}')
-
-                # Сброс всех банкнот в нижнюю кассету
-                if keyboard.is_pressed('X'):
-                    device.sspEmpty()
-
                 # Проверка состояния устройства и получение событий
-                device.poll()
+                dev.poll()
 
             time.sleep(0.100)
         except Exception as e:
             logger.error(f'Произошла ошибка: {e}')
-            if device:
-                device.disconnect()
-
+            if dev:
+                dev.disconnect()
     # Отключение устройства
-    device.disconnect()
+    dev.disconnect()
 
+
+def sample():
+    global device, is_app_exit
+
+    print(f'Используемый порт: {itl_dev_port}')
+    # Создание объекта устройства
+    device = Device(event_callback=device_events_callback,  port=itl_dev_port)
+
+    thread = Thread(target=worker, args=(device,))
+    thread.start()
+
+    while not is_app_exit:
+        try:
+            print(f'Введите:')
+            print(f'"E" для включение приема банкнот')
+            print(f'"D" для выключения приема банкнот')
+            print(f'"P" для выдачи суммы')
+            print(f'"Z" для сброса банкнот свыше установленного количества')
+            print(f'"R" для отчета о состоянии устройства')
+            print(f'"X" для сброса всех банкнот в кассету')
+            print()
+            print(f'Введите "~" для выхода из программы')
+            print()
+
+            user_command = input("Введите команду: ").strip().upper()
+
+            is_app_exit = user_command == '~'
+            if is_app_exit:
+                print('Выход из программы...')
+                break
+            elif user_command == 'E':
+                # Включение приема банкнот
+                print('Включение приема банкнот')
+                device.sspEnableValidator()
+            elif user_command == 'D':
+                # Выключение приема банкнот
+                print('Выключение приема банкнот')
+                device.sspDisableValidator()
+            elif user_command == 'P':
+                # Выдача суммы
+                amount = get_number(input('Введите сумму для выдачи: '))
+                print(f'Выдача суммы: {amount}')
+                device.sspPayout(amount, SSP_DEFAULT_CURRENCY)
+            elif user_command == 'Z':
+                # Сброс банкноты
+                amount = get_number(input('Введите номинал банкноты: '))
+                count = get_number(input('Введите количество банкнот для сдачи: '))
+                device.sspFloatByDenomination({amount: count})
+            elif user_command == 'R':
+                # Отчет о состоянии устройства
+                for banknote_nominal in SSP_DEFAULT_ACCEPT_LIMIT:
+                    # Запрос количества банкнот для сдачи
+                    res, counter = device.sspGetNoteAmount(banknote_nominal, SSP_DEFAULT_CURRENCY)
+                    if res == SSPResponse.ok:
+                       print(f'Количество банкнот {banknote_nominal} {SSP_DEFAULT_CURRENCY}: {counter}')
+                # Запрос информации о последней операции перемещения банкнот в кассету
+                res, note_info = device.sspGetCashboxPayoutOpData()
+                if res == SSPResponse.ok:
+                    for banknote_nominal, banknote_count in note_info.items():
+                        print(f'Количество банкнот {banknote_nominal} {SSP_DEFAULT_CURRENCY}: {banknote_count}')
+            elif user_command == 'X':
+                # Сброс всех банкнот в нижнюю кассету
+                device.sspEmpty()
+
+        except Exception as e:
+            logger.error(f'Произошла ошибка: {e}')
+
+    thread.join()
 
 if __name__ == "__main__":
     # test_smpayout()
